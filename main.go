625a76bf936b3fe095211b187a0ddbb5e0d64545
Samantha Wu <samanthayywu@gmail.com>
2022-11-22 15:15:48 +0000

    feedback

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func winelistHandler(w http.ResponseWriter, r *http.Request) {
-
+	json.NewEncoder(w).Encode("This is the winelist API")
 }
 
 func main() {
