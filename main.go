195c29237eaae590ea338e1094334d97980d4f8f
Samantha Wu <samanthayywu@gmail.com>
2022-11-22 15:12:46 +0000

    println

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func winelistHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -8,6 +11,7 @@ func winelistHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/winelist", winelistHandler)
+	log.Println("API is running")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
