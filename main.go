a4a99c95f3cc66a4a5ca36ebda4f59ef63ffaddc
Samantha Wu <samanthayywu@gmail.com>
2022-11-22 14:58:22 +0000

    setup

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main()
+import "net/http"
+
+func winelistHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	http.HandleFunc("/winelist", winelistHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+
+}
