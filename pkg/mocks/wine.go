f925e5e1a2c8f1a72e898812f14bc78051eac10d
Samantha Wu <samanthayywu@gmail.com>
2022-11-25 13:40:59 +0000

    mocks

---
+++ pkg/mocks/wine.go
@@ -0,0 +1,13 @@
+package mocks
+
+import "github.com/syywu/go-api.git/pkg/models"
+
+var Wines = []models.Wine{
+	{
+		Producer:    "Olivier Leflaive",
+		Name:        "St Aubin 1er Cru Dents de Chien",
+		Region:      "France",
+		Rating:      92,
+		Description: "Gorgeous nose of buttery lemon, orange zest, honeysuckle, vanilla and melon. The texture is crisp and refreshing, with a vanilla caramel density shot through with lime. Creaminess and oak underpin the fruit.",
+	},
+}
