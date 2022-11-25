b3f15e6767dcbafc44b9aaf909376713ec419406
Samantha Wu <samanthayywu@gmail.com>
2022-11-25 12:49:35 +0000

    wines obj

--- main.go
+++ main.go
@@ -16,6 +16,16 @@ type Wine struct {
 	description  string `json:"desc"`
 }
 
+var Wines = []Wine{
+	{
+		producerName: "Olivier Leflaive",
+		name:         "St Aubin 1er Cru Dents de Chien",
+		region:       "France",
+		expertRating: 92,
+		description:  "Gorgeous nose of buttery lemon, orange zest, honeysuckle, vanilla and melon. The texture is crisp and refreshing, with a vanilla caramel density shot through with lime. Creaminess and oak underpin the fruit.",
+	},
+}
+
 func winelistHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("This is the winelist API")
 }
