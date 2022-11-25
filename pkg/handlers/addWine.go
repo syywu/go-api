32df88d325cf389105207f81cab34f9e47408100
Samantha Wu <samanthayywu@gmail.com>
2022-11-25 17:19:55 +0000

    AddWine req body

--- pkg/handlers/addWine.go
+++ pkg/handlers/addWine.go
@@ -1,9 +1,26 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/syywu/go-api.git/pkg/models"
+)
 
 func AddWine(w http.ResponseWriter, r *http.Request) {
 	// read req body
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var wine models.Wine
+	json.Unmarshal(body, &wine)
+
 	// add to wines []
 	// send back 201 status code
 }
