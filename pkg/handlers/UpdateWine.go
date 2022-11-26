1a0a7298c77506aadb63539fb090b1858949e60e
Samantha Wu <samanthayywu@gmail.com>
2022-11-26 15:09:13 +0000

    updateWine plan

--- pkg/handlers/UpdateWine.go
+++ pkg/handlers/UpdateWine.go
@@ -3,5 +3,8 @@ package handlers
 import "net/http"
 
 func UpdateWine(w http.ResponseWriter, r *http.Request) {
-
+	// read dynamic id
+	//read req body
+	// iterates over wines
+	// update and send res back when id matches
 }
