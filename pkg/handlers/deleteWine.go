25822746ab8d2bac27e1631dfc80c8cefed68230
Samantha Wu <samanthayywu@gmail.com>
2022-11-26 21:47:56 +0000

    deleteWine

--- pkg/handlers/deleteWine.go
+++ pkg/handlers/deleteWine.go
@@ -1,7 +1,23 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/syywu/go-api.git/pkg/mocks"
+)
 
 func DeleteWine(w http.ResponseWriter, r *http.Request) {
+	// read dynamic id
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
 
+	// loops over mocks
+	for index, wine := range mocks.Wines {
+		if wine.Id == id {
+			// send response and delete book
+			mocks.Wines = append(mocks.Wines[:index], mocks.Wines[index+1:]...)
+		}
+	}
 }
