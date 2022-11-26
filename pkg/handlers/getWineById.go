319e08d52e29a2c7593f68a71fd189f6eac4fad2
Samantha Wu <samanthayywu@gmail.com>
2022-11-26 14:15:22 +0000

    GetWineById

--- pkg/handlers/getWineById.go
+++ pkg/handlers/getWineById.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/syywu/go-api.git/pkg/mocks"
 )
 
 func GetWineById(w http.ResponseWriter, r *http.Request) {
@@ -13,5 +15,13 @@ func GetWineById(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.Atoi(vars["id"])
 
 	// iterate over mock.wines
-	// if the same then return wine
+	for _, wine := range mocks.Wines {
+		if wine.Id == id {
+			// if the same then return wine
+			w.WriteHeader(http.StatusOK)
+			w.Header().Add("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(wine)
+			break
+		}
+	}
 }
