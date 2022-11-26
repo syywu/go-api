c0c67121da262ee559e8ef23e45cc5a131402652
Samantha Wu <samanthayywu@gmail.com>
2022-11-26 14:15:52 +0000

    GetWineById res

--- pkg/handlers/getWineById.go
+++ pkg/handlers/getWineById.go
@@ -21,7 +21,6 @@ func GetWineById(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			w.Header().Add("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(wine)
-			break
 		}
 	}
 }
