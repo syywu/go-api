4e39f511e52e9d5f8e964a4e0427318837faec0d
Samantha Wu <samanthayywu@gmail.com>
2022-11-26 14:24:47 +0000

    UpdateWine

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/winelist", handlers.GetAllWines).Methods(http.MethodGet)
 	router.HandleFunc("/winelist/{id}", handlers.GetWineById)
+	router.HandleFunc("/winelist/{id}", handlers.UpdateWine)
 	router.HandleFunc("/winelist", handlers.AddWine).Methods(http.MethodPost)
 
 	log.Println("API is running")
