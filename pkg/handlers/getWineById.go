d520c110c431b700af7f04bf54b50fe35b1b294e
Samantha Wu <samanthayywu@gmail.com>
2022-11-26 14:01:33 +0000

    read dynamic id

--- pkg/handlers/getWineById.go
+++ pkg/handlers/getWineById.go
@@ -1,9 +1,17 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
 
 func GetWineById(w http.ResponseWriter, r *http.Request) {
 	// read id
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
 	// iterate over mock.wines
 	// if the same then return wine
 }
