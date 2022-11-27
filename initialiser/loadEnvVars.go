e98899c40de0c00f36b78b777c5c1c3529231c45
Samantha Wu <samanthayywu@gmail.com>
2022-11-27 13:13:08 +0000

    initialiser

---
+++ initialiser/loadEnvVars.go
@@ -0,0 +1,14 @@
+package initialiser
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
