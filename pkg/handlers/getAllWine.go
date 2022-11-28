ca689def5dd3c573e49fbaba0d453bd419ab4f78
Samantha Wu <samanthayywu@gmail.com>
2022-11-28 12:18:37 +0000

    getAll db

--- pkg/handlers/getAllWine.go
+++ pkg/handlers/getAllWine.go
@@ -2,13 +2,20 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
-	"github.com/syywu/go-api.git/pkg/mocks"
+	"github.com/syywu/go-api.git/pkg/models"
 )
 
 func (h handler) GetAllWines(w http.ResponseWriter, r *http.Request) {
+	var wine []models.Wine
+
+	if res := h.DB.Find(&wine); res.Error != nil {
+		fmt.Println(res.Error)
+	}
+
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(mocks.Wines)
+	json.NewEncoder(w).Encode(&wine)
 }
