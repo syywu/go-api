3c765690698aa00606abd44d801100a1cda4a9ab
Samantha Wu <samanthayywu@gmail.com>
2022-11-28 12:35:00 +0000

    update db

--- pkg/handlers/UpdateWine.go
+++ pkg/handlers/UpdateWine.go
@@ -2,13 +2,13 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
-	"github.com/syywu/go-api.git/pkg/mocks"
 	"github.com/syywu/go-api.git/pkg/models"
 )
 
@@ -28,22 +28,23 @@ func (h handler) UpdateWine(w http.ResponseWriter, r *http.Request) {
 	var updatedWine models.Wine
 	json.Unmarshal(body, &updatedWine)
 
-	// iterates over wines
-	for index, wine := range mocks.Wines {
-		if wine.Id == id {
-			// update and send res back when id matches
-			wine.Producer = updatedWine.Producer
-			wine.Name = updatedWine.Name
-			wine.Region = updatedWine.Region
-			wine.Rating = updatedWine.Rating
-			wine.Description = updatedWine.Description
-			// update mocks
-			mocks.Wines[index] = wine
+	var wine models.Wine
 
-			w.Header().Add("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode("Updated")
-
-		}
+	if res := h.DB.First(&wine, id); res.Error != nil {
+		fmt.Println(res.Error)
 	}
+
+	// update and send res back when id matches
+	wine.Producer = updatedWine.Producer
+	wine.Name = updatedWine.Name
+	wine.Region = updatedWine.Region
+	wine.Rating = updatedWine.Rating
+	wine.Description = updatedWine.Description
+
+	h.DB.Save(&wine)
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("Updated")
+
 }
