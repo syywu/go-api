f94a429d92ef320ea9f8176725d28ce82e6bed2f
Samantha Wu <samanthayywu@gmail.com>
2022-11-28 11:53:38 +0000

    addWine db

--- pkg/handlers/addWine.go
+++ pkg/handlers/addWine.go
@@ -2,12 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"net/http"
 
-	"github.com/syywu/go-api.git/pkg/mocks"
 	"github.com/syywu/go-api.git/pkg/models"
 )
 
@@ -24,8 +23,9 @@ func (h handler) AddWine(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(body, &wine)
 
 	// add to wines []
-	wine.Id = rand.Intn(100)
-	mocks.Wines = append(mocks.Wines, wine)
+	if res := h.DB.Create(&wine); res.Error != nil {
+		fmt.Println(res.Error)
+	}
 
 	// send back 201 status code
 	w.Header().Add("Content-Type", "application/json")
